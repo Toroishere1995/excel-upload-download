6a92d342dd217149306a7fabd8fa695815c1741c
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-10 13:14:47 +0530

    Project Setup

---
+++ src/test/java/com/assignment/exceluploaddownload/ExcelUploadDownloadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment.exceluploaddownload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcelUploadDownloadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
