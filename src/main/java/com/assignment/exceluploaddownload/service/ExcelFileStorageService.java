a8845f4da8b1187f68175dc42276d244da1176b2
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-11 13:28:45 +0530

    ExcelData persistence removed;Download func implemented.

--- src/main/java/com/assignment/exceluploaddownload/service/ExcelFileStorageService.java
+++ src/main/java/com/assignment/exceluploaddownload/service/ExcelFileStorageService.java
@@ -7,19 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.assignment.exceluploaddownload.dto.ExcelDataDto;
-import com.assignment.exceluploaddownload.entity.ExcelData;
 import com.assignment.exceluploaddownload.entity.ExcelFile;
 import com.assignment.exceluploaddownload.payload.UploadResponse;
-import com.assignment.exceluploaddownload.repository.ExcelDataRepository;
 import com.assignment.exceluploaddownload.repository.ExcelFileRepository;
 import com.assignment.exceluploaddownload.util.ExcelFileProcessor;
 
 @Service
 public class ExcelFileStorageService {
 
-	@Autowired
-	private ExcelDataRepository excelDataRepository;
+	/*
+	 * @Autowired private ExcelDataRepository excelDataRepository;
+	 */
 
 	@Autowired
 	private ExcelFileRepository excelFileRepository;
@@ -30,12 +28,8 @@ public class ExcelFileStorageService {
 			UploadResponse uploadResponse = new UploadResponse();
 			ExcelFile excelFile = storeExcelFile(uploadedFile);
 			uploadResponse.setFilename(excelFile.getName());
-			ArrayList<ExcelDataDto> columns = ExcelFileProcessor.convertExcelToEntity(uploadedFile);
-			for (ExcelDataDto column : columns) {
-				storeExcelDataValues(column, excelFile);
-				uploadResponse.getColoumnHeadings().add(column.getColumnHeading());
-			}
-
+			ArrayList<String> columns = ExcelFileProcessor.retrieveHeadersFromExcelFile(excelFile.getContent());
+			uploadResponse.setColoumnHeadings(columns);
 			response.add(uploadResponse);
 		}
 		return response;
@@ -48,14 +42,20 @@ public class ExcelFileStorageService {
 		return excelFileRepository.saveAndFlush(excelFile);
 	}
 
-	private ExcelData storeExcelDataValues(ExcelDataDto excelDataDto, ExcelFile excelFile) {
-		ExcelData excelData = new ExcelData();
-		String[] columnValues = new String[excelDataDto.getColumnValues().size()];
-		columnValues = excelDataDto.getColumnValues().toArray(columnValues);
-		excelData.setColumnValues(columnValues);
-		excelData.setColumnHeading(excelDataDto.getColumnHeading());
-		excelData.setExcelFile(excelFile);
-		excelFile.getData().add(excelData);
-		return excelDataRepository.save(excelData);
+	/*
+	 * private ExcelData storeExcelDataValues(ExcelDataDto excelDataDto, ExcelFile
+	 * excelFile) { ExcelData excelData = new ExcelData(); String[] columnValues =
+	 * new String[excelDataDto.getColumnValues().size()]; columnValues =
+	 * excelDataDto.getColumnValues().toArray(columnValues);
+	 * excelData.setColumnValues(columnValues);
+	 * excelData.setColumnHeading(excelDataDto.getColumnHeading());
+	 * excelData.setExcelFile(excelFile); excelFile.getData().add(excelData); return
+	 * excelDataRepository.save(excelData); }
+	 */
+	public void downloadFile(String file1Id, String file2Id) {
+		ExcelFile excelFile = excelFileRepository.getOne(Long.parseLong(file1Id));
+		ExcelFile excelFile2 = excelFileRepository.getOne(Long.parseLong(file2Id));
+		String columnOrder[] = { "ID", "NAME", "FAME", "GAME", "DI", "LASTNAME" };
+		ExcelFileProcessor.concatenateTwoFilesWithOrder(columnOrder, excelFile.getContent(), excelFile2.getContent());
 	}
 }
