3ef454bec22836dec185007b04b313232ef5d018
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-11 13:27:21 +0530

    Removed persistence of Excel Class.

--- src/main/java/com/assignment/exceluploaddownload/entity/ExcelData.java
+++ src/main/java/com/assignment/exceluploaddownload/entity/ExcelData.java
@@ -1,32 +1,32 @@
 package com.assignment.exceluploaddownload.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
+/*
 @Entity
 @Table(name = "EXCEL_DATA")
+*/
 public class ExcelData {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	/*
+	 * @Id
+	 * 
+	 * @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 */
 	private Long id;
 
 	private String columnHeading;
 
 	private String[] columnValues;
 
-	@ManyToOne(optional = false, fetch = FetchType.LAZY)
-	@JoinColumn(name = "EXCEL_FILES_ID")
-	private ExcelFile excelFile;
+	/*
+	 * @ManyToOne(optional = false, fetch = FetchType.LAZY)
+	 * 
+	 * @JoinColumn(name = "EXCEL_FILES_ID") private ExcelFile excelFile; public
+	 * ExcelFile getExcelFile() { return excelFile;
+	 * 
+	 * }
+	 * 
+	 * public void setExcelFile(ExcelFile excelFile) { this.excelFile = excelFile; }
+	 */
 
 	public String[] getColumnValues() {
 		return columnValues;
@@ -34,10 +34,6 @@ public class ExcelData {
 
 	public String getColumnHeading() {
 		return columnHeading;
-	}
-
-	public ExcelFile getExcelFile() {
-		return excelFile;
 	}
 
 	public Long getId() {
@@ -50,10 +46,6 @@ public class ExcelData {
 
 	public void setColumnHeading(String columnHeading) {
 		this.columnHeading = columnHeading;
-	}
-
-	public void setExcelFile(ExcelFile excelFile) {
-		this.excelFile = excelFile;
 	}
 
 	public void setId(Long id) {
--- src/main/java/com/assignment/exceluploaddownload/repository/ExcelDataRepository.java
+++ src/main/java/com/assignment/exceluploaddownload/repository/ExcelDataRepository.java
@@ -1,9 +1,5 @@
 package com.assignment.exceluploaddownload.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assignment.exceluploaddownload.entity.ExcelData;
-
-public interface ExcelDataRepository extends JpaRepository<ExcelData, Long> {
+public interface ExcelDataRepository { // extends JpaRepository<ExcelData, Long> {
 
 }
