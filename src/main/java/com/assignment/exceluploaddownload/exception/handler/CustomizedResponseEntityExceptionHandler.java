badecb67dfbe70840ae9a00a7834650365542355
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-12 11:44:38 +0530

    Implemented MultipartFile Exception

--- src/main/java/com/assignment/exceluploaddownload/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/assignment/exceluploaddownload/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.assignment.exceluploaddownload.exception.ExcelFileNotFoundException;
@@ -15,15 +16,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
 	@ExceptionHandler(value = ExcelFileNotFoundException.class)
 	public ResponseEntity<ResponsePayload> resourceException(ExcelFileNotFoundException exception) {
-		ResponsePayload payload = new ResponsePayload("404", exception.getMessage(),
-				null, "RESOURCE NOT FOUND");
+		ResponsePayload payload = new ResponsePayload("404", exception.getMessage(), null, "RESOURCE NOT FOUND");
 		return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(value = ExcelFileProcessingException.class)
-	public ResponseEntity<ResponsePayload> excelFileSupportException(ExcelFileProcessingException exception){
-		ResponsePayload payload = new ResponsePayload("415", exception.getMessage(),
-				null, "UNSUPPORTED FILE TYPE");
+	public ResponseEntity<ResponsePayload> excelFileSupportException(ExcelFileProcessingException exception) {
+		ResponsePayload payload = new ResponsePayload("415", exception.getMessage(), null, "UNSUPPORTED FILE TYPE");
+		return new ResponseEntity<>(payload, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+	}
+
+	@ExceptionHandler(value = MultipartException.class)
+	public ResponseEntity<ResponsePayload> fileSizeException(MultipartException exception) {
+		ResponsePayload payload = new ResponsePayload("415", "Either no file or file size issue.", null, "UNSUPPORTED FILE TYPE");
 		return new ResponseEntity<>(payload, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
 	}
 }
